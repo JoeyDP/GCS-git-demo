628dffda37d0542aa4a20b1039b6a0ff48ae3491
joey <joeydepauw@live.be>
2017-11-12 14:33:29 +0100

    better fib

--- fib.py
+++ fib.py
@@ -2,10 +2,10 @@ import sys
 
 
 def fib(n):
-	if n < 2:
-		return 1
-	else:
-		return fib(n-1) + fib(n-2)
+	fib_nums = [1, 1]
+	for i in range(2, n):
+		fib_nums.append(fib_nums[i-1] + fib_nums[i-2])
+	return fib_nums[-1]
 
 
 print(fib(int(sys.argv[1])))
